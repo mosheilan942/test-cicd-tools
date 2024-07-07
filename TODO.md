0f9e7b80698991892041d3694a297d50cf13c773
moshe ilan <moisi6510@gmail.com>
2024-07-07 21:22:49 +0300

    test 17 with npx lint-staged

---
+++ TODO.md
@@ -0,0 +1,2 @@
+if we want to create the eslint file manually:
+"npx mrm@2 lint-staged"
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 // import { useEffect } from "react";
-import './App.css';
+import "./App.css";
 
 // const dedo = new Promise((resolve, reject) => {
 // 	if (viteLogo) {
@@ -88,20 +88,18 @@ import './App.css';
 // }
 // console.log(foo);
 
-const foo = () => {
-	const a = 1;
-	const b = 2;
-	const c = 3;
-	const d = 4;
-	const e = 5;
-	const f = 6;
-	const g = 7;
-	const h = 8;
-	const i = 9;
-	console.log(a, b, c, d, e, f, g, h, i);
-};
-console.log('foo');
-console.log(foo);
+// const foo = () => {
+// 	const a = 1;
+// 	const b = 2;
+// 	const c = 3;
+// 	const d = 4;
+// 	const e = 5;
+// 	const f = 6;
+// 	const g = 7;
+// 	const h = 8;
+// 	const i = 9;
+// 	console.log(a, b, c, d, e, f, g, h, i);
+// };
 
 // class foo {
 // 	foo: string;
@@ -125,46 +123,42 @@ console.log(foo);
 // var foo = 1;
 
 const array = [{ bar: { baz: 100 } }, 2, 3];
-const object = { foo: 'bar' };
 
 // const foo1 = array[0];
 bar.baz = array;
-const [, , foo76] = array;
 
-const foo2 = object.foo;
-const foo3 = object['foo'];
-
-const [foo4] = array;
 // const [{ bar: { baz } }] = array;
 // [bar.baz] = array;
 
 // With `object` enabled
 // const { foo1 } = object;
 
-console.log(foo2, foo3, foo4, foo);
-
 function App() {
-	const [count, setCount] = useState(0);
-	return (
-		<>
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-		</>
-	);
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
 }
 
 export default App;
